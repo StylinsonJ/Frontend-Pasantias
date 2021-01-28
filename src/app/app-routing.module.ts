f47dbd27b8f9ff1220113425ed6419af69c73872
StylinsonJ <jassyra.espinoza@tecsup.edu.pe>
2021-01-28 18:37:13 -0500

    jwt-login-dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { NavComponent } from './nav/nav.component';
 import { ContabilidadComponent } from './modulos/contabilidad/contabilidad.component';
-import { DefaultComponent } from './default/default.component'
-import { TableroComponent } from './modulos/tablero/tablero.component';
 
 const routes: Routes = [{
-  path:'', 
-  component: NavComponent,
-  children: [{
-    path: '',
-    component: TableroComponent
-    },{
-    path: 'contabilidad', 
-    component: ContabilidadComponent
-  }]
+  path:'contabilidad', component: ContabilidadComponent
+  
 }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { TableroComponent } from './modulos/tablero/tablero.component';
+import { NavComponent } from './nav/nav.component';
+import { ContabilidadComponent } from './modulos/contabilidad/contabilidad.component';
+import { AlmacenComponent } from './modulos/almacen/almacen.component';
+import { FinanzasComponent } from './modulos/finanzas/finanzas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AlmacenComponent,
+    ContabilidadComponent,
+    FinanzasComponent,
+    TableroComponent,
   ],
   imports: [
     BrowserModule,
